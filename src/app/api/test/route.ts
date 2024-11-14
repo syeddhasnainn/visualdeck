2b3c8e937047eb189b7630e9c02033894daa5e3a
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2024-11-14 22:25:54 +0000

    first deploy

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,11 +1,12 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { getCloudflareContext } from "@opennextjs/cloudflare";
 
-export async function POST(request: NextRequest) {
-  const { query } = await request.json() as any || { query: "" };
+export async function POST(request: Request) {
+  const { userQuery } = await request.json() as { userQuery: string };
+  
   const { env } = await getCloudflareContext();
   const queryVector = await env.AI.run("@cf/baai/bge-base-en-v1.5", {
-    text: query,
+    text: userQuery,
   });
 
   let matches = await env.VECTORIZE.query(queryVector.data[0], {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import { useState } from "react";
 export default function Home() {
-  const [query, setQuery] = useState("");
+  const [userQuery, setUserQuery] = useState("");
   const [matches, setMatches] = useState([]);
   const [loading, setLoading] = useState(false);
 
@@ -11,20 +11,20 @@ export default function Home() {
     setMatches([]);
     const res = await fetch(`/api/test`, {
       method: "POST",
-      body: JSON.stringify({ query })
+      body: JSON.stringify({ userQuery })
     });
     const data = await res.json() as any;
     const m = data.matches.matches;
     setMatches(m);
     setLoading(false);
-    setQuery("");
+    setUserQuery("");
   };
 
   return (
     <div>
       <div>
-        <div className="mx-auto max-w-[90%]">
-          <input onKeyDown={(e) => e.key === "Enter" && handleSearch()} value={query} onChange={(e) => setQuery(e.target.value)} placeholder="Search for a frame" className="w-full outline-none border border-gray-600 rounded-xl p-2 bg-transparent my-8" type="text" />
+        <div className="mx-auto max-w-[90%] my-8 flex flex-col gap-4">
+          <input onKeyDown={(e) => e.key === "Enter" && handleSearch()} value={userQuery} onChange={(e) => setUserQuery(e.target.value)} placeholder="Search for a frame" className="w-full outline-none border border-gray-600 rounded-xl p-2 bg-transparent" type="text" />
           {loading ? (
             <div className="flex justify-center my-8">
               <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-600"></div>
@@ -32,7 +32,7 @@ export default function Home() {
           ) : (
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 rounded-2xl">
               {matches.map((m: any) => (
-                <div key={m}>
+                <div className="rounded-xl overflow-hidden" key={m}>
                   <Image className="w-full" src={m.metadata.img_url} alt="image" width={500} height={500} />
                 </div>
               ))}
