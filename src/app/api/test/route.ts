65bbf3bb24d6e93aa43f925e28093f2818eb19ac
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2024-11-14 18:20:48 +0000

    init-working

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,8 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getCloudflareContext } from "@opennextjs/cloudflare";
 
-export const runtime = 'edge';
-
 export async function POST(request: NextRequest) {
   const { query } = await request.json() as any || { query: "" };
   const { env } = await getCloudflareContext();
